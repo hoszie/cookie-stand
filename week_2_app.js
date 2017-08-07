78d0bcf87aa4912d3b7d53ddf992102c72d263d0
Nick Hoszko <nhoszko@gmail.com>
2017-08-07 13:53:00 -0700

    store_objects

---
+++ week_2_app.js
@@ -0,0 +1,30 @@
+
+var pdxAirport = {
+    minCust: 23,
+    maxCust: 65,
+    cookieAvg: 6.3,
+}
+
+var pioneerSquare = {
+    minCust: 3,
+    maxCust: 24,
+    cookieAvg: 1.2,
+}
+
+var powells = {
+    minCust: 11,
+    maxCust: 38,
+    cookieAvg: 3.7,
+}
+
+var stJohns = {
+    minCust: 20,
+    maxCust: 38,
+    cookieAvg: 2.3,
+}
+
+var waterFront = {
+    minCust: 2,
+    maxCust: 16,
+    cookieAvg: 4.6,
+}
